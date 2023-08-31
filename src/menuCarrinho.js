635ba136a44ce8a3ca5f94f2f12649659ba12f30
Jeanne <jeannefernanda.m@gmail.com>
2023-08-30 20:09:54 -0400

    Ajustes estéticos e edicionar itens no carrinho

--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -1,3 +1,5 @@
+import { catalogo } from "./utilidades";
+
 function abrirCarrinho() {
     document.getElementById("carrinho").classList.remove("right-[-360px]");
     document.getElementById("carrinho").classList.add("right-[0px]");
@@ -14,4 +16,19 @@ export function inicializarCarrinho() {
 
     botaoFecharCarrinho.addEventListener("click", fecharCarrinho);
     botaoAbrirCarrinho.addEventListener("click", abrirCarrinho);
+}
+
+export function adcionarAoCarrinho(idProduto) {
+    const produto = catalogo.find(p => p.id === idProduto);
+    const containerProdutoCarrinho = document.getElementById('produtos-carrinho');
+    const cartaoProdutoCarrinho = ` <article class="flex bg-slate-100 rounded-lg p-1 relative">
+    <button id="fechar-carrinho" class="absolute top-0 right-2"><i class="fa-solid fa-circle-xmark text-slate-500 hover:text-slate-800"></i></button>
+    <img src="./assets/img/${produto.imagem}" alt="Carrinho: ${produto.nome}" class="h-24 rounded-lg">
+    <div class="py-2">
+      <p class="text-slate-900 text-sm">${produto.nome}</p>
+      <p class="text-slate-400 text-xm">Tamanho: M</p>
+      <p class="text-green-700 text-lg">$${produto.preco}</p>
+    </div>      
+  </article>`;
+  containerProdutoCarrinho.innerHTML += cartaoProdutoCarrinho;
 }
